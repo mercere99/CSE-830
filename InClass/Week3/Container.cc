ebc839ac3bd772868ee330d245a38b0ac775d836
Charles Ofria <charles.ofria@gmail.com>
2020-09-14 00:52:26 -0400

    Added a SortedList container.

--- InClass/Week3/Container.cc
+++ InClass/Week3/Container.cc
@@ -199,6 +199,37 @@ public:
 };
 
 
+class SortedList : public ActiveContainer {
+private:
+  std::list<int> vals;
+  std::list<int>::iterator cur_it;
+  
+public:
+  bool Search(int test_val) override {
+    // Check every value for a match, stopping when they get too high.
+    for (cur_it = vals.begin();
+         cur_it != vals.end() && *cur_it < test_val;
+         cur_it++);
+    return *cur_it == test_val;
+  }
+  void Insert(int in_val) override {
+    Search(in_val);                 // Find the closest position to in_val.
+    vals.insert(cur_it, in_val);    // And insert it there.
+  }
+  void Delete() override { vals.erase(cur_it); }
+
+  int Min() override { cur_it = vals.begin(); return *cur_it; }
+  int Max() override { cur_it = --vals.end(); return *cur_it; }
+    
+  int Next() override { return *(++cur_it); }
+  int Prev() override { return *(--cur_it); }
+  
+  bool HasCurrent() override { return cur_it != vals.end(); }
+  int Current() override { return HasCurrent() ? *cur_it : 0; }
+  int GetSize() override { return vals.size(); }
+};
+
+
 template <typename T>
 void InsertMany(T & container, const std::vector<int> & values) {
   for (int v : values) container.Insert(v);
