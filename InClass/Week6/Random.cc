94e39516a66cb8cf3b81018b23e160fb899911c5
Charles Ofria <charles.ofria@gmail.com>
2020-10-20 21:46:07 -0400

    Added a TestDice() function and other cleanup.

--- InClass/Week6/Random.cc
+++ InClass/Week6/Random.cc
@@ -2,6 +2,7 @@
 #include <unordered_set>
 #include <string>
 #include <sstream>
+#include <vector>
 
 struct MiddleSquare {
   size_t seed;
@@ -79,21 +80,42 @@ size_t TestCycle(size_t seed) {
     hash = rng.GetHash();
   }
 
-  return found.size();
+  return val;
 }
+
+template <typename T>
+std::string TestDice(size_t seed) {
+  std::vector<int> rolls(6, 0);
+  T rng(seed);
+  for (size_t i = 0; i < 1000000; i++) {
+    int val = rng.Get() % 6;
+    rolls[val]++;
+  }
+
+  std::stringstream ss;
+  for (size_t i = 0; i < 6; i++) {
+    if (i) ss << " ";
+    ss << i << ":" << rolls[i];
+  }
+
+  return ss.str();
+}
+
 
 template <typename T>
 void Test(const std::string & name, size_t seed) {
   std::cout << name << ":" << std::endl;
   std::cout << "  Example Vals: " << GetValues<T>(seed) << std::endl;
   std::cout << "  First Repeat: " << TestRepeat<T>(seed) << std::endl;
-  std::cout << "  Cycle Size:   " << TestCycle<T>(seed) << std::endl;
+  std::cout << "  Cycle Start:  " << TestCycle<T>(seed) << std::endl;
   std::cout << "  Example Dice: " << GetValues<T>(seed, 30, 6) << std::endl;
+  std::cout << "  Dice Summary: " << TestDice<T>(seed) << std::endl;
 }
 
 int main()
 {
-  size_t seed = 675248;
+  // size_t seed = 675248;
+  size_t seed = 123456;
 
   Test<MiddleSquare>("MiddleSquare", seed);
   Test<LCG>("LCG", seed);
